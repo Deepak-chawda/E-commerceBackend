bd06ef4d4cf540501c43de785f0851d88168a775
Deepak chawda <deepakchawda96969@gmail.com>
2021-09-15 00:01:47 +0530

    work on user.controller.js

--- controller/user/user.controller.js
+++ controller/user/user.controller.js
@@ -10,7 +10,9 @@ exports.signinController = async (req, res) => {
   try {
     const user = await UserModal.findOne({ email: req.body.email });
     if (!user)
-      return res.json({ error: "Invalid user mail ", data: null, code: 404 });
+      return res
+        .status(404)
+        .json({ error: "Invalid user mail ", data: null, code: 404 });
 
     // check password & compare
     // console.log(user.password)
@@ -20,7 +22,9 @@ exports.signinController = async (req, res) => {
     );
     // console.log(comparePassword)
     if (!comparePassword)
-      return res.json({ error: "Invalid password ", data: null, code: 404 });
+      return res
+        .status(400)
+        .json({ error: "Invalid password ", data: null, code: 404 });
 
     // jwt.sign({ foo: 'bar' }, privateKey, { algorithm: 'RS256' }, function(err, token) {
     //   console.log(token);
@@ -28,11 +32,12 @@ exports.signinController = async (req, res) => {
 
     //  here i am creating token asynchronously
     const token = await jwt.sign(
-      { _id: user._id.toString()},
+      { _id: user._id.toString() },
       process.env.SECRET_KEY
     );
-
-    res.json({ data: { user, token }, error: null, code: 200 });
+    // don't send password for front end
+    user.password = undefined;
+    res.status(200).json({ data: { user, token }, error: null, code: 200 });
   } catch (error) {
     console.log("error => ", error);
     res.json({ error: "Something went wrong", data: null, code: 500 });
@@ -43,7 +48,7 @@ exports.signinController = async (req, res) => {
 exports.signupController = async (req, res) => {
   const isAdmin = req.query.isAdmin; //tue, false
   // because isAdmin return to string type so you need to conver into boolean type
-  const $isAdmin = isAdmin=== "true" ? true : false;
+  const $isAdmin = isAdmin === "true" ? true : false;
   try {
     if (!$isAdmin) {
       const newUser = new UserModal({
@@ -51,9 +56,14 @@ exports.signupController = async (req, res) => {
         password: req.body.password,
         role: "USER",
       });
-      // if (UserModal.exists({ email : req.body.email })) {
-      //   return res.json({err : "This email already present in database",data: null, code: 404,});
-      // }
+      const validateEmail = await UserModal.exists({ email: req.body.email });
+      if (validateEmail) {
+        return res.json({
+          error: "This email already present in database",
+          data: null,
+          code: 404,
+        });
+      }
       // this is a sync function below convert in async function
       // bcrypt.genSalt(10, function(err, salt) {
       //     bcrypt.hash("B4c0/\/", salt, function(err, hash) {
